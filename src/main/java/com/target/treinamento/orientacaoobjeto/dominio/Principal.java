cf586d404013df8c825578e6400d97038f1c6193
Rafael_Bauer <comercialrafaelbauer@gmail.com>
2019-01-25 20:17:32 -0200

    Inserido método construtor para o objeto empregado

--- src/main/java/com/target/treinamento/orientacaoobjeto/dominio/Principal.java
+++ src/main/java/com/target/treinamento/orientacaoobjeto/dominio/Principal.java
@@ -14,21 +14,10 @@ public class Principal {
 	
 	private void setEmpregado() {
 		
-		Empregado e1 = new Empregado();
-		e1.setNome("João");
-		e1.setCargo("Gerente");
-		e1.setSalario(15000.00);
-		
-		Empregado e2 = new Empregado();
-		e2.setNome("João");
-		e2.setCargo("Analista");
-		e2.setSalario(5000.00);
-		
-		Empregado e3 = new Empregado();
-		e3.setNome("João");
-		e3.setCargo("Estagiario");
-		e3.setSalario(1000.00);
-		
+		Empregado e1 = new Empregado("João", "Gerente", 15000.00);
+		Empregado e2 = new Empregado("João", "Gerente", 15000.00);
+		Empregado e3 = new Empregado("João", "Gerente", 15000.00);
+
 		System.out.println(e1.getNome() +" "+ e1.getCargo() +" "+ e1.getSalario());
 		System.out.println(e2.getNome() +" "+ e2.getCargo() +" "+ e2.getSalario());
 		System.out.println(e3.getNome() +" "+ e3.getCargo() +" "+ e3.getSalario());
